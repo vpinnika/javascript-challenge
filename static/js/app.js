5139e5d596e085fb6cedcd0724fc454d2c90976f
vpinnika <venkat.pinnika@gmail.com>
2020-04-21 20:54:13 -0700

    Adding .html to master path

---
+++ static/js/app.js
@@ -0,0 +1,49 @@
+// from data.js
+var tableData = data;
+var tbody = d3.select("tbody");
+
+// Display the entire dataset as default
+
+tableData.forEach((report) => {
+    console.log(report);
+    var row = tbody.append('tr');
+
+    Object.entries(report).forEach(([key, value]) => {
+        console.log(key, value);
+        var cell = row.append('td');
+        cell.text(value);
+    });
+});
+
+// Select the submit button
+var submit = d3.select("#filter-btn");
+
+//Click event of datetime filter
+submit.on("click", function() {
+
+// Remove existing table
+d3.select("tbody").html("");
+
+// Prevent the page from refreshing
+d3.event.preventDefault();
+
+// Get the value property of the input element
+var dateTime = d3.select("#datetime").property("value");
+console.log(dateTime);
+
+// Filter reports
+var filteredData = tableData.filter(record => record.datetime === dateTime);
+console.log(filteredData);
+
+// Display the filtered dataset
+filteredData.forEach((report) => {
+    var row = tbody.append('tr');
+
+    Object.entries(report).forEach(([key, value]) => {
+        console.log(key, value);
+        var cell = row.append('td');
+        cell.text(value);
+    });
+});
+
+});
